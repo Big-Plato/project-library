56b65057da52f0ef67cbc2c42fec83f1589fd38e
Big-Plato <luisoctavius.sc@gmail.com>
2024-12-31 19:00:15 -0300

    The add button does not work yet

--- index.js
+++ index.js
@@ -1,103 +1,92 @@
-
 const myLibrary = [];
 
-const container = document.querySelector('.container');
-const dialog = document.querySelector('dialog');
-const showDialogBtn = document.querySelector('dialog + button');
-const closeDialogBtn = document.querySelector('#close');
-const submitBtn = document.querySelector('#submit');
+const container = document.querySelector(".container");
+const dialog = document.querySelector("dialog");
+const showDialogBtn = document.querySelector("dialog + button");
+const closeDialogBtn = document.querySelector("#close");
+const submitBtn = document.querySelector("#submit");
 const form = document.querySelector('#book-form');
+const card = document.querySelector('.card');
+
+formData = new FormData(form);
 
 console.log(form);
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    let bookTitle = document.querySelector('#title');
-    let authorBook = document.querySelector('#author');
-    let yearBook = document.querySelector('#year')
+  let bookTitle = document.querySelector("#title");
+  let authorBook = document.querySelector("#author");
+  let yearBook = document.querySelector("#year");
 
-    let arrBook = [bookTitle.value, authorBook.value, yearBook.value];
-    console.log(arrBook)
-    addBookToLibrary(myLibrary, arrBook);
+  let arrBook = [bookTitle.value, authorBook.value, yearBook.value];
+  console.log(arrBook)
+  console.log(formData.getAll(bookTitle))
+  addBookToLibrary(myLibrary, arrBook);
 
-    // if (bookTitle.value == "" || authorBook.value == "" || yearBook.title == "") {
-    //     throw new Error("Please, fill in before submit") 
-    // } else {
-    //     console.log(bookTitle.value);
-    // }
-
-    console.log(e);
-})
-
-
-showDialogBtn.addEventListener('click', () => {
-    dialog.showModal();
+  // if (bookTitle.value == "" || authorBook.value == "" || yearBook.title == "") {
+  //     throw new Error("Please, fill in before submit")
+  // } else {
+  //     console.log(bookTitle.value);
+  // }
 });
 
-closeDialogBtn.addEventListener('click', () => {
-    dialog.close();
-    console.log("Hi");
+showDialogBtn.addEventListener("click", () => {
+  dialog.showModal();
 });
 
+closeDialogBtn.addEventListener("click", () => {
+  dialog.close();
+  console.log("Hi");
+});
 
 // submitBtn.addEventListener('submit', (e) => {
 //     console.log(e)
 //     e.preventDefault();
 // })
 
-function Book(title, author, year, read) {
-    this.title = title;
-    this.author = author;
-    this.year = year;
-    this.read = read;
+function Book(title, author, year) {
+  this.title = title;
+  this.author = author;
+  this.year = year;
+
 }
 
-const book1 = new Book('Apology of Socrates', 'Plato', 1988, true);
-const book2 = new Book('O Idiota', 'Dostoiévski', 1888, false);
+const book1 = new Book("Apology of Socrates", "Plato", 1988);
+const book2 = new Book("O Idiota", "Dostoiévski", 1888);
 
-function addBookToLibrary(arr, Book) {
-    arr.push(Book);
+function addBookToLibrary(arr) {
+  for (let i = card.length; i > 0; i--) {
+    let add = Object.create(Book);
+    console.log
+  }
 }
 
 addBookToLibrary(myLibrary, book1);
-addBookToLibrary(myLibrary, book2)
+addBookToLibrary(myLibrary, book2);
 
+function displayBooks(arr) {
+  let i = 0;
+  while (i < arr.length) {
+    const itemListBook = document.createElement("div");
+    itemListBook.classList.add("card");
+    container.appendChild(itemListBook);
+    const para1 = document.createElement("p");
+    const para2 = document.createElement("p");
+    const para3 = document.createElement("p");
+    itemListBook.appendChild(para1);
+    itemListBook.appendChild(para2);
+    itemListBook.appendChild(para3);
 
-function displayBooks (arr) {
-    let i = 0;
-    while (i < arr.length) {
-        const itemListBook = document.createElement('div');
-        itemListBook.classList.add('card');
-        container.appendChild(itemListBook);
-        const para1 = document.createElement('p');
-        const para2 = document.createElement('p');
-        const para3 = document.createElement('p');
-        const para4 = document.createElement('p');
-        itemListBook.appendChild(para1);
-        itemListBook.appendChild(para2);
-        itemListBook.appendChild(para3);
-        itemListBook.appendChild(para4);
+    para1.textContent = `${arr[i].title}`;
+    para2.textContent = `${arr[i].author}`;
+    para3.textContent = `${arr[i].year}`;
 
-
-        para1.textContent = `Title: 
-        ${arr[i].title}`;
-        para2.textContent = `Author:
-        ${arr[i].author}`;
-        para3.textContent = `Year:
-        ${arr[i].year}`;  
-        
-        if (read === true) {
-            para4.textContent = "Read"
-        } else if (read === false) {
-            para4.textContent = "Not read"
-        }
-
-        console.log(arr[i].title);
-        console.log(arr[i].author);
-        console.log(arr[i].year);
-        i++;
-    }
+    console.log(arr[i].title);
+    console.log(arr[i].author);
+    console.log(arr[i].year);
+    i++;
+  }
 }
 
 displayBooks(myLibrary);
